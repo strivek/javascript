49833bd7af016f4627739cbddfefda7b4c6974bd
GaoFei <strivek@163.com>
2014-10-23 14:23:12 +0800

    尝试dom集合

--- dom/demo.js
+++ dom/demo.js
@@ -7,13 +7,11 @@ window.onload = function() {
   var list = document.querySelectorAll(".item");
 
 
-  alert(list[0].style);
+  alert(list[0].stylew);
 
   function dwn(content) {
     document.write(content + "<br />");
-
   }
-
   for (var c in list) {
     document.write("属性名:" + c + "------------属性值:" + list[c] + "<br />");
   }
